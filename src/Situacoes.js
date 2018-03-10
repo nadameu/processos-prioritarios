ff197c52867270b0ba0d5b30f04324a39c1a7372
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2018-03-10 01:37:22 -0300

    Refactoring Situações (estética)

--- src/Situacoes.js
+++ src/Situacoes.js
@@ -1,7 +1,8 @@
-export default {
-	INICIAL: 1,
-	'MOVIMENTO-AGUARDA DESPACHO': 2,
-	MOVIMENTO: 3,
-	'MOVIMENTO-AGUARDA SENTENÇA': 4,
-	INDEFINIDA: 5,
-};
+const Situacoes = {};
+Situacoes['INICIAL'] = 1;
+Situacoes['MOVIMENTO-AGUARDA DESPACHO'] = 2;
+Situacoes['MOVIMENTO'] = 3;
+Situacoes['MOVIMENTO-AGUARDA SENTENÇA'] = 4;
+Situacoes['INDEFINIDA'] = 5;
+
+export default Situacoes;
