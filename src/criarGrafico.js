62454b2933be4880e2dd4ce7a2d631f6cc100d78
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2018-03-10 02:43:17 -0300

    Refactoring

--- src/criarGrafico.js
+++ src/criarGrafico.js
@@ -1,19 +1,17 @@
 import Grafico from './Grafico';
 
 function excluirCanvasAntigo() {
-	const canvases = document.getElementsByTagName('canvas');
+	const canvases = Array.from(document.getElementsByTagName('canvas'));
 	if (canvases.length > 0) {
 		console.log('Excluindo canvas antigo');
-		Array.from(canvases).forEach(canvas =>
-			canvas.parentNode.removeChild(canvas)
-		);
+		canvases.forEach(canvas => canvas.parentNode.removeChild(canvas));
 	}
 }
 
 function extrairProcessos(localizadores) {
 	const processos = new Map();
-	const agora = new Date(),
-		hoje = new Date(agora.getFullYear(), agora.getMonth(), agora.getDate());
+	const agora = new Date();
+	const hoje = new Date(agora.getFullYear(), agora.getMonth(), agora.getDate());
 	localizadores.forEach(localizador => {
 		localizador.processos.forEach(processo => {
 			const numproc = processo.numproc;
@@ -38,8 +36,8 @@ function extrairProcessos(localizadores) {
 
 function extrairDatas(processos) {
 	const datas = new Map();
-	for (let timestamp of processos.values()) {
-		let valorAtual = datas.get(timestamp) || 0;
+	for (const timestamp of processos.values()) {
+		const valorAtual = datas.get(timestamp) || 0;
 		datas.set(timestamp, valorAtual + 1);
 	}
 	return datas;
