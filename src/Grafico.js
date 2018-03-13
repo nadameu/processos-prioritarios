00fd196e5c8b49bd75007ad37fbc3ea47fa64125
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2018-03-13 16:52:33 -0300

    Tipos

--- src/Grafico.js
+++ src/Grafico.js
@@ -1,80 +1,168 @@
+//@ts-check
+
 import { apenasData } from './helpers';
 
+/**
+ * @typedef Area
+ * @prop {string} corFundo
+ * @prop {Linha}  linha
+ * @prop {Margens} margens
+ * @prop {DimensoesArea} dimensoes
+ */
+
+/**
+ * @typedef Barras
+ * @prop {number} espacamento
+ * @prop {string} corVencido
+ * @prop {string} corProximosDias
+ * @prop {string} corNoPrazo
+ * @prop {{(): number}} largura
+ */
+
+/**
+ * @typedef Categorias
+ * @prop {{(): number}} distancia
+ * @prop {number} quantidade
+ */
+
+/**
+ * @typedef DimensoesArea
+ * @prop {number} altura
+ * @prop {number} largura
+ */
+
+/**
+ * @typedef DimensoesGrafico
+ * @prop {{(): number}} largura
+ * @prop {number} altura
+ * @prop {number} margem
+ * @prop {number} espacamento
+ */
+
+/**
+ * @typedef Escala
+ * @prop {number} largura
+ * @prop {number} maximo
+ * @prop {number} unidadePrimaria
+ * @prop {number} unidadeSecundaria
+ * @prop {Linha} linhaPrimaria
+ * @prop {Linha} linhaSecundaria
+ */
+
+/**
+ * @typedef Linha
+ * @prop {number} espessura
+ * @prop {string} cor
+ */
+
+/**
+ * @typedef Margens
+ * @prop {number} t
+ * @prop {number} r
+ * @prop {number} b
+ * @prop {number} l
+ */
+
+/**
+ * @typedef Texto
+ * @prop {number} altura
+ * @prop {string} cor
+ * @prop {string} corSecundaria
+ */
+
 export default class Grafico {
-	get area() {
-		var area = {
-			corFundo: 'rgba(102, 102, 102, 0.25)',
-			linha: { espessura: 1, cor: '#666' },
-		};
+	/** @return {Area} */
+	area() {
+		const espessuraLinha = 1;
 		const margemExterna =
 			this.dimensoes.margem +
 			this.linha.espessura / 2 +
 			this.dimensoes.espacamento +
-			area.linha.espessura / 2;
-		area.margens = {
+			espessuraLinha / 2;
+		const margens = {
 			t: margemExterna + this.texto.altura / 2,
 			r: margemExterna,
 			b: margemExterna + this.texto.altura + this.dimensoes.espacamento,
 			l: margemExterna + this.escala.largura + 2 * this.dimensoes.espacamento,
 		};
-		area.dimensoes = {
-			largura: this.dimensoes.largura - area.margens.l - area.margens.r,
-			altura: this.dimensoes.altura - area.margens.t - area.margens.b,
+		const dimensoes = {
+			largura: this.dimensoes.largura() - margens.l - margens.r,
+			altura: this.dimensoes.altura - margens.t - margens.b,
 		};
-		return area;
+		return {
+			corFundo: 'rgba(102, 102, 102, 0.25)',
+			linha: { espessura: espessuraLinha, cor: '#666' },
+			margens,
+			dimensoes,
+		};
 	}
-	constructor() {
-		this.dimensoes = {
-			get largura() {
-				return Math.min(
-					1024,
-					document.querySelector('#divInfraAreaTelaD').clientWidth
-				);
-			},
-			altura: 400,
-			margem: 3,
-			espacamento: 5,
-		};
-		this.linha = { espessura: 1, cor: 'rgba(255, 255, 255, 0.9)' };
-		this.corFundo = 'rgb(51, 51, 51)';
-		this.texto = {
-			altura: 10,
-			cor: 'hsla(180, 100%, 87%, 0.87)',
-			corSecundaria: 'hsla(180, 100%, 87%, 0.5)',
-		};
-		this.escala = {
-			maximo: 20,
-			unidadePrimaria: 10,
-			unidadeSecundaria: 5,
-			largura: 2 * this.texto.altura,
-			linhaPrimaria: { espessura: 2, cor: '#888' },
-			linhaSecundaria: { espessura: 0.5, cor: '#666' },
-		};
-		let self = this;
-		this.categorias = {
-			quantidade: 1,
-			get distancia() {
-				return self.area.dimensoes.largura / self.categorias.quantidade;
-			},
-		};
-		this.barras = {
-			corVencido: 'hsla(15, 80%, 75%, 1)',
-			corProximosDias: 'hsla(60, 100%, 75%, 1)',
-			corNoPrazo: 'hsla(120, 75%, 80%, 1)',
-			espacamento: 0.2,
+	/** @type {DimensoesGrafico} */
+	dimensoes = {
+		largura() {
+			return Math.min(
+				1024,
+				document.querySelector('#divInfraAreaTelaD').clientWidth
+			);
+		},
+		altura: 400,
+		margem: 3,
+		espacamento: 5,
+	};
+	/** @type {Linha} */
+	linha = { espessura: 1, cor: 'rgba(255, 255, 255, 0.9)' };
+	/** @type {string} */
+	corFundo = 'rgb(51, 51, 51)';
+	/** @type {Texto} */
+	texto = {
+		altura: 10,
+		cor: 'hsla(180, 100%, 87%, 0.87)',
+		corSecundaria: 'hsla(180, 100%, 87%, 0.5)',
+	};
+	/** @type {Escala} */
+	escala = {
+		maximo: 20,
+		unidadePrimaria: 10,
+		unidadeSecundaria: 5,
+		largura: 2 * this.texto.altura,
+		linhaPrimaria: { espessura: 2, cor: '#888' },
+		linhaSecundaria: { espessura: 0.5, cor: '#666' },
+	};
+	/** @type {Categorias} */
+	categorias = {
+		quantidade: 1,
+		distancia() {
+			return this.area().dimensoes.largura / this.categorias.quantidade;
+		},
+	};
+	/** @type {Barras} */
+	barras = {
+		corVencido: 'hsla(15, 80%, 75%, 1)',
+		corProximosDias: 'hsla(60, 100%, 75%, 1)',
+		corNoPrazo: 'hsla(120, 75%, 80%, 1)',
+		espacamento: 0.2,
 
-			/* valor entre 0 e 1, proporcional à largura disponível */
-			get largura() {
-				return self.categorias.distancia * (1 - self.barras.espacamento);
-			},
-		};
+		/* valor entre 0 e 1, proporcional à largura disponível */
+		largura() {
+			return this.categorias.distancia * (1 - this.barras.espacamento);
+		},
+	};
+	/** @type {HTMLCanvasElement} */
+	canvas;
+	/** @type {CanvasRenderingContext2D} */
+	context;
+	/** @type {Map<number, number>} */
+	dados;
+	constructor() {
 		const canvas = document.createElement('canvas');
-		canvas.width = this.dimensoes.largura;
+		canvas.width = this.dimensoes.largura();
 		canvas.height = this.dimensoes.altura;
 		this.canvas = canvas;
 		this.context = this.canvas.getContext('2d');
 		this.dados = new Map();
 	}
+	/**
+	 * @param {Map<number, number>} dados
+	 */
 	inserirDados(dados) {
 		this.dados = dados;
 	}
@@ -92,11 +180,11 @@ export default class Grafico {
 	desenharFundo() {
 		const context = this.context;
 		context.fillStyle = this.corFundo;
-		context.fillRect(0, 0, this.dimensoes.largura, this.dimensoes.altura);
+		context.fillRect(0, 0, this.dimensoes.largura(), this.dimensoes.altura);
 		context.beginPath();
 		let x = this.dimensoes.margem;
 		let y = x;
-		let w = this.dimensoes.largura - 2 * this.dimensoes.margem;
+		let w = this.dimensoes.largura() - 2 * this.dimensoes.margem;
 		let h = this.dimensoes.altura - 2 * this.dimensoes.margem;
 		context.rect(x, y, w, h);
 		context.lineWidth = this.linha.espessura;
@@ -107,15 +195,15 @@ export default class Grafico {
 		const context = this.context;
 		context.beginPath();
 		context.rect(
-			this.area.margens.l,
-			this.area.margens.t,
-			this.area.dimensoes.largura,
-			this.area.dimensoes.altura
+			this.area().margens.l,
+			this.area().margens.t,
+			this.area().dimensoes.largura,
+			this.area().dimensoes.altura
 		);
-		context.fillStyle = this.area.corFundo;
+		context.fillStyle = this.area().corFundo;
 		context.fill();
-		context.lineWidth = this.area.linha.espessura;
-		context.strokeStyle = this.area.linha.cor;
+		context.lineWidth = this.area().linha.espessura;
+		context.strokeStyle = this.area().linha.cor;
 		context.stroke();
 	}
 	desenharEscala() {
@@ -127,7 +215,7 @@ export default class Grafico {
 			this.escala.largura / 2;
 		const xLinha =
 			xTexto + this.escala.largura / 2 + this.dimensoes.espacamento;
-		const wLinha = this.area.dimensoes.largura + this.dimensoes.espacamento;
+		const wLinha = this.area().dimensoes.largura + this.dimensoes.espacamento;
 		for (
 			let i = 0;
 			i <= this.escala.maximo;
@@ -145,8 +233,8 @@ export default class Grafico {
 			let proporcao = i / this.escala.maximo;
 			let y =
 				this.dimensoes.altura -
-				this.area.margens.b -
-				proporcao * this.area.dimensoes.altura;
+				this.area().margens.b -
+				proporcao * this.area().dimensoes.altura;
 			context.fillText(i.toString(), xTexto, y);
 			context.beginPath();
 			context.moveTo(xLinha, y);
@@ -159,13 +247,15 @@ export default class Grafico {
 		const larguraPossivelTexto = context.measureText('99').width;
 		const step = Math.ceil(
 			(larguraPossivelTexto + this.dimensoes.espacamento) /
-				this.categorias.distancia
+				this.categorias.distancia()
 		);
 		const hoje = apenasData(Date.now());
 		context.fillStyle = this.texto.cor;
 		const y =
 			this.dimensoes.altura -
-			(this.dimensoes.margem + this.linha.espessura / 2 + this.area.margens.b) /
+			(this.dimensoes.margem +
+				this.linha.espessura / 2 +
+				this.area().margens.b) /
 				2;
 		for (let i = 0; i < this.categorias.quantidade; i += step) {
 			let dia = new Date(
@@ -173,7 +263,7 @@ export default class Grafico {
 				hoje.getMonth(),
 				hoje.getDate() + i
 			);
-			let x = this.area.margens.l + (i + 0.5) * this.categorias.distancia;
+			let x = this.area().margens.l + (i + 0.5) * this.categorias.distancia();
 			context.fillText(dia.getDate().toString(), x, y);
 		}
 	}
@@ -181,7 +271,7 @@ export default class Grafico {
 		const context = this.context;
 		const hoje = apenasData(Date.now());
 		const larguraBarra =
-			this.categorias.distancia * (1 - this.barras.espacamento);
+			this.categorias.distancia() * (1 - this.barras.espacamento);
 		for (let i = 0; i < this.categorias.quantidade; i++) {
 			if (i === 0) {
 				context.fillStyle = this.barras.corVencido;
@@ -197,12 +287,12 @@ export default class Grafico {
 			);
 			if (this.dados.has(dia.getTime())) {
 				let x =
-					this.area.margens.l +
-					(i + 0.5) * this.categorias.distancia -
+					this.area().margens.l +
+					(i + 0.5) * this.categorias.distancia() -
 					larguraBarra / 2;
 				let valor = this.dados.get(dia.getTime());
-				let altura = valor / this.escala.maximo * this.area.dimensoes.altura;
-				let y = this.dimensoes.altura - this.area.margens.b - altura;
+				let altura = valor / this.escala.maximo * this.area().dimensoes.altura;
+				let y = this.dimensoes.altura - this.area().margens.b - altura;
 				context.fillRect(x, y, larguraBarra, altura);
 			}
 		}
@@ -266,7 +356,7 @@ export default class Grafico {
 				Math.ceil(this.escala.maximo / novoIntervalo) * novoIntervalo;
 			if (
 				novoMaximo / novoIntervalo * distancia <=
-				this.area.dimensoes.altura
+				this.area().dimensoes.altura
 			) {
 				tamanhoIdealEncontrado = true;
 				if (mult !== 1) {
