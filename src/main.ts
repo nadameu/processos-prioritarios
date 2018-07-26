56d6e016dc43f46aa99c41c40d11c0724c0932d3
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2018-07-26 02:40:24 -0300

    Refactoring

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,31 @@
 import { Either, Left, Right } from '../adt/Either';
-import { LocalizadoresFactory } from './LocalizadoresFactory';
-import { queryOne } from './Query/queryOne';
 import adicionarBotaoComVinculo from './adicionarBotaoComVinculo';
+import { Localizadores, LocalizadoresFactory } from './LocalizadoresFactory';
+import { queryOne } from './Query/queryOne';
 
-export default function main(doc: Document): Either<Error, string | void> {
-	const url = new URL(doc.URL);
-	if (
-		url.searchParams.get('acao') ===
-		'usuario_tipo_monitoramento_localizador_listar'
-	) {
-		return queryOne<HTMLTableElement>('#divInfraAreaTabela table', doc)
-			.map(LocalizadoresFactory.fromTabela)
+export default function main(doc: Document): Either<Error, string> {
+	const analisar = (
+		parentSelector: string,
+		factory: (_: HTMLTableElement) => Localizadores
+	): Either<Error, string> =>
+		queryOne<HTMLTableElement>(`${parentSelector} table`, doc)
+			.map(tbl => Right(tbl) as Either<Error, HTMLTableElement>)
+			.getOrElse(Left(new Error('Tabela não localizada.')))
+			.map(factory)
 			.chain(adicionarBotaoComVinculo);
+
+	const url = new URL(doc.URL);
+	const params = url.searchParams;
+	const acao = params.get('acao');
+	if (acao === 'usuario_tipo_monitoramento_localizador_listar') {
+		return analisar('#divInfraAreaTabela', LocalizadoresFactory.fromTabela);
 	}
-	if (url.searchParams.get('acao') === 'localizador_processos_lista') {
+	if (acao === 'localizador_processos_lista') {
 		return Right('Nada a fazer.');
 	}
-	if (url.searchParams.get('acao_origem') === 'principal') {
-		return queryOne<HTMLTableElement>('#fldLocalizadores table', doc)
-			.map(LocalizadoresFactory.fromTabelaPainel)
-			.chain(adicionarBotaoComVinculo);
+	const acaoOrigem = params.get('acao_origem');
+	if (acaoOrigem === 'principal') {
+		return analisar('#fldLocalizadores', LocalizadoresFactory.fromTabelaPainel);
 	}
-	return Left(
-		new Error(`Ação desconhecida: "${url.searchParams.get('acao')}".`)
-	);
+	return Left(new Error(`Ação desconhecida: "${acao}".`));
 }
