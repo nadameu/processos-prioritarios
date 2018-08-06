82c727fb2aba3e6941bb902873c08235b6f50768
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2018-08-06 18:29:16 -0300

    Update

--- adt/LazyLinkedList.test.ts
+++ adt/LazyLinkedList.test.ts
@@ -1,97 +1,227 @@
-import * as L3 from './LazyLinkedList';
+import * as L from './LazyLinkedList';
 
-test('chain', () => {
+describe('chain', () => {
 	const values = [10, 20, 30];
 	const f = (x: number) => [x, x + 2, x + 3];
-	const expected = fromArray(values.map(f).reduce((acc, xs) => acc.concat(xs), [] as number[]));
-	const list = L3.chain(L3.fromIterable(values), x => L3.fromIterable(f(x)));
-	const actual = fromLazyList(list);
-	expect(actual).toEqual(expected);
+	const expected = values.map(f).reduce((acc, xs) => acc.concat(xs), [] as number[]);
+	const list = L.fromIterable(values);
+	test('data first', () => {
+		const actual = fromLazyList(L.chain(list, x => L.fromIterable(f(x))));
+		expect(actual).toEqual(expected);
+	});
+	test('data last', () => {
+		const actual = pipeline(list, L.chain(x => L.fromIterable(f(x))), fromLazyList);
+		expect(actual).toEqual(expected);
+	});
 });
 
 test('concat', () => {
 	const a = [1, 2, 3];
 	const b = [4, 5, 6];
-	const expected = fromArray(a.concat(b));
-	const list = L3.concat(L3.fromIterable(a), L3.fromIterable(b));
+	const expected = a.concat(b);
+	const list = L.concat(L.fromIterable(a), L.fromIterable(b));
 	const actual = fromLazyList(list);
 	expect(actual).toEqual(expected);
+});
+
+test('empty', () => {
+	expect(fromLazyList(L.empty())).toEqual([]);
+});
+
+test('fromArguments', () => {
+	expect(fromLazyList(L.fromArguments())).toEqual([]);
+	expect(fromLazyList(L.fromArguments(42))).toEqual([42]);
+	expect(fromLazyList(L.fromArguments(1, 2, 3, 4, 5))).toEqual([1, 2, 3, 4, 5]);
 });
 
 test('fromArrayLike', () => {
 	const arrayLike = { 0: 1, 1: 2, 2: 3, 3: 4, 4: 5, length: 5 };
-	const expected = fromArray(Array.from(arrayLike));
-	const list = L3.fromArrayLike(arrayLike);
+	const expected = Array.from(arrayLike);
+	const list = L.fromArrayLike(arrayLike);
 	const actual = fromLazyList(list);
 	expect(actual).toEqual(expected);
+});
+
+describe('fromGenerator', () => {
+	test('can be called multiple times', () => {
+		const generator = function*() {
+			yield 1;
+			yield 2;
+			yield 3;
+			yield 4;
+			yield 5;
+		};
+		const listA = L.fromGenerator(generator);
+		const listB = L.fromGenerator(generator);
+		expect(fromLazyList(listA)).toEqual(fromLazyList(listB));
+	});
 });
 
 test('fromIterable', () => {
-	const generator: () => Iterable<number> = function*() {
-		yield 1;
-		yield 2;
-		yield 3;
-		yield 4;
-		yield 5;
+	const iterable: Iterable<number> = {
+		*[Symbol.iterator]() {
+			yield 1;
+			yield 2;
+			yield 3;
+			yield 4;
+			yield 5;
+		},
 	};
-	const expected = fromArray(Array.from(generator()));
-	const list = L3.fromIterable(generator());
+	const expected = Array.from(iterable);
+	const list = L.fromIterable(iterable);
 	const actual = fromLazyList(list);
 	expect(actual).toEqual(expected);
 });
 
-test('map', () => {
+describe('map', () => {
 	const values = [1, 2, 3, 4, 5];
 	const f = (x: number) => x * 3;
 	const g = (x: number) => x + 2;
-	const expected = fromArray(values.map(f).map(g));
-	const actual = fromLazyList(L3.map(L3.map(L3.fromIterable(values), f), g));
-	expect(actual).toEqual(expected);
+	const expected = values.map(f).map(g);
+	const list = L.fromIterable(values);
+	test('data first', () => {
+		const actual = fromLazyList(L.map(L.map(list, f), g));
+		expect(actual).toEqual(expected);
+	});
+	test('data last', () => {
+		const actual = pipeline(list, L.map(f), L.map(g), fromLazyList);
+		expect(actual).toEqual(expected);
+	});
 });
 
-test('reduce', () => {
-	type Result<A> = Next<A> | Done;
-	type Next<A> = { isDone: false; prev: Result<A>; value: A };
-	type Done = { isDone: true };
-	const Done: Done = { isDone: true };
-	const Next = <A>(prev: Result<A>, value: A): Next<A> => ({ isDone: false, prev, value });
-	const values = [1, 2, 3, 4, 5];
-	const expected = values.reduce<Result<number>>(Next, Done);
-	const list = L3.fromIterable(values);
-	const actual = L3.reduce<number, Result<number>>(list, Next, Done);
-	expect(actual).toEqual(expected);
+describe('match', () => {
+	const ok = [1, 2, 3, 4].reduceRight<L.LazyLinkedList<number>>(
+		(next, value) => () => L.LazyCons(value, next),
+		() => L.Nil
+	);
+	const nil: L.LazyLinkedList<number> = () => L.Nil;
+	const f = (x: number, y: number) => x * 2 + y + 1;
+	const seed = 8;
+	const calc = (
+		recFoldr: <A, B>(list: L.LazyLinkedList<A>, f: (acc: B, _: A) => B, seed: B) => B,
+		list: L.LazyLinkedList<number>
+	): number => recFoldr(list, f, seed);
+	test('data first', () => {
+		const recFoldr = <A, B>(list: L.LazyLinkedList<A>, f: (acc: B, _: A) => B, seed: B): B => {
+			return L.match(list, {
+				Nil: () => seed,
+				LazyCons: (value, next) => f(recFoldr(next, f, seed), value),
+			});
+		};
+		expect(calc(recFoldr, ok)).toEqual(192);
+		expect(calc(recFoldr, nil)).toEqual(8);
+	});
+	test('data last', () => {
+		const recFoldr = <A, B>(list: L.LazyLinkedList<A>, f: (acc: B, _: A) => B, seed: B): B => {
+			return L.match<A, B>({
+				Nil: () => seed,
+				LazyCons: (value, next) => f(recFoldr(next, f, seed), value),
+			})(list);
+		};
+		expect(calc(recFoldr, ok)).toEqual(192);
+		expect(calc(recFoldr, nil)).toEqual(8);
+	});
+});
+
+test('of', () => {
+	expect(fromLazyList(L.of(42))).toEqual([42]);
+});
+
+describe('reduce', () => {
+	const ok = [1, 2, 3, 4].reduceRight<L.LazyLinkedList<number>>(
+		(next, value) => () => L.LazyCons(value, next),
+		() => L.Nil
+	);
+	const nil: L.LazyLinkedList<number> = () => L.Nil;
+	const f = (x: number, y: number) => x * 2 + y + 1;
+	const seed = 8;
+	test('data first', () => {
+		expect(L.reduce(ok, f, seed)).toEqual(169);
+		expect(L.reduce(nil, f, seed)).toEqual(8);
+	});
+	test('data last', () => {
+		expect(pipeline(ok, L.reduce(f, seed))).toEqual(169);
+		expect(pipeline(nil, L.reduce(f, seed))).toEqual(8);
+	});
 });
 
 test('toArray', () => {
 	const expected = [1, 2, 3, 4, 5];
-	const list = L3.fromIterable(expected);
-	const actual = L3.toArray(list);
+	const list = L.fromIterable(expected);
+	const actual = L.toArray(list);
 	expect(actual).toEqual(expected);
 });
 
-type LinkedList<A> = Cons<A> | Nil;
-interface Cons<A> {
-	0: A;
-	1: LinkedList<A>;
-	length: 2;
-}
-type Nil = 0;
-const Nil: Nil = 0;
-const Cons = <A>(value: A, next: LinkedList<A>): Cons<A> => [value, next];
-function fromLazyList<A>(list: L3.LazyLinkedList<A>): LinkedList<A> {
-	let head: LinkedList<A> = 0;
-	let previousLink: LinkedList<A> = 0;
-	let currentLink: LinkedList<A> = 0;
+describe('toIterable', () => {
+	const array = [1, 2, 3, 4, 5];
+	const list = L.toIterable(L.fromArrayLike(array));
+	test('Array.from', () => {
+		expect(Array.from(list)).toEqual(array);
+	});
+	test('Symbol.iterator', () => {
+		const iterArray = array[Symbol.iterator]();
+		const iterList = list[Symbol.iterator]();
+		let resultArray: IteratorResult<number>;
+		let resultList: IteratorResult<number>;
+		do {
+			resultArray = iterArray.next();
+			resultList = iterList.next();
+			expect(resultList).toEqual(resultArray);
+		} while (!resultArray.done && !resultList.done);
+	});
+	test('for ... of', () => {
+		let i = 0;
+		for (const x of list) {
+			expect(x).toEqual(array[i++]);
+		}
+	});
+});
+
+describe('unsafeReduceRight', () => {
+	const createIterable = (limit: number) => ({
+		*[Symbol.iterator]() {
+			let i = 1;
+			while (i <= limit) {
+				yield i++;
+			}
+			console.log('nope');
+		},
+	});
+	const ok = [1, 2, 3, 4].reduceRight<L.LazyLinkedList<number>>(
+		(next, value) => () => L.LazyCons(value, next),
+		() => L.Nil
+	);
+	const nil: L.LazyLinkedList<number> = () => L.Nil;
+	const f = (x: number, y: number) => x * 2 + y + 1;
+	const seed = 8;
+	test('data first', () => {
+		expect(L.unsafeReduceRight(ok, f, seed)).toEqual(192);
+		expect(L.unsafeReduceRight(nil, f, seed)).toEqual(8);
+	});
+	test('data last', () => {
+		expect(pipeline(ok, L.unsafeReduceRight(f, seed))).toEqual(192);
+		expect(pipeline(nil, L.unsafeReduceRight(f, seed))).toEqual(8);
+	});
+	test('is not stack safe', () => {
+		const list = L.fromIterable(createIterable(1e5));
+		expect(() => L.unsafeReduceRight(list, (_, last) => last, 0)).toThrow();
+	});
+});
+
+function fromLazyList<A>(list: L.LazyLinkedList<A>): A[] {
+	let array = [];
 	let current = list();
-	while (current !== L3.Nil) {
-		currentLink = Cons(current[0], Nil);
-		if (previousLink !== Nil) previousLink[1] = currentLink;
-		if (head === Nil) head = previousLink;
-		previousLink = currentLink;
+	while (current !== L.Nil) {
+		array.push(current[0]);
 		current = current[1]();
 	}
-	return head;
+	return array;
 }
-function fromArray<A>(array: A[]): LinkedList<A> {
-	return array.reduceRight<LinkedList<A>>((prev, value) => [value, prev], 0);
+
+type Fn<A, B> = (_: A) => B;
+function pipeline<A, B>(a: A, f: Fn<A, B>): B;
+function pipeline<A, B, C>(a: A, f: Fn<A, B>, g: Fn<B, C>): C;
+function pipeline<A, B, C, D>(a: A, f: Fn<A, B>, g: Fn<B, C>, h: Fn<C, D>): D;
+function pipeline(initial: any, ...fs: Function[]) {
+	return fs.reduce((res, f) => f(res), initial);
 }
--- adt/LazyLinkedList.ts
+++ adt/LazyLinkedList.ts
@@ -9,7 +9,18 @@ export const Nil: Nil = 0;
 export function LazyCons<A>(value: A, next: LazyLinkedList<A>): LazyCons<A> {
 	return [value, next];
 }
+export function match<A, B>(def: {
+	Nil(): B;
+	LazyCons(value: A, next: LazyLinkedList<A>): B;
+}): (list: LazyLinkedList<A>) => B;
 export function match<A, B>(
+	list: LazyLinkedList<A>,
+	def: { Nil(): B; LazyCons(value: A, next: LazyLinkedList<A>): B }
+): B;
+export function match() {
+	return _purry(_match, arguments);
+}
+function _match<A, B>(
 	list: LazyLinkedList<A>,
 	def: { Nil(): B; LazyCons(value: A, next: LazyLinkedList<A>): B }
 ): B {
@@ -17,8 +28,39 @@ export function match<A, B>(
 	return result === Nil ? def.Nil() : def.LazyCons(result[0], result[1]);
 }
 
-export function fromArrayLike<A>(as: ArrayLike<A>): LazyLinkedList<A> {
-	return () => _fromArrayLike(as);
+export function chain<A, B>(
+	f: (_: A) => LazyLinkedList<B>
+): (list: LazyLinkedList<A>) => LazyLinkedList<B>;
+export function chain<A, B>(
+	list: LazyLinkedList<A>,
+	f: (_: A) => LazyLinkedList<B>
+): LazyLinkedList<B>;
+export function chain() {
+	return _purry(_chain, arguments);
+}
+function _chain<A, B>(list: LazyLinkedList<A>, f: (_: A) => LazyLinkedList<B>): LazyLinkedList<B> {
+	return () =>
+		_match(list, {
+			Nil: () => Nil,
+			LazyCons: (value, next) => concat(f(value), _chain(next, f))(),
+		});
+}
+
+export function concat<A>(listA: LazyLinkedList<A>, listB: LazyLinkedList<A>): LazyLinkedList<A> {
+	return () =>
+		_match(listA, { Nil: listB, LazyCons: (value, next) => LazyCons(value, concat(next, listB)) });
+}
+
+export function empty<A = never>(): LazyLinkedList<A> {
+	return () => Nil;
+}
+
+export function fromArguments<A>(...xs: A[]): LazyLinkedList<A> {
+	return () => _fromArrayLike(xs);
+}
+
+export function fromArrayLike<A>(arrayLike: ArrayLike<A>): LazyLinkedList<A> {
+	return () => _fromArrayLike(arrayLike);
 }
 function _fromArrayLike<A>(
 	as: ArrayLike<A>,
@@ -28,41 +70,43 @@ function _fromArrayLike<A>(
 	return i < len ? LazyCons(as[i], () => _fromArrayLike(as, len, i + 1)) : Nil;
 }
 
-export function fromIterable<A>(as: Iterable<A>): LazyLinkedList<A> {
-	return () => _fromIterable(as[Symbol.iterator]());
+export function fromGenerator<A>(generator: () => Iterator<A>): LazyLinkedList<A> {
+	return () => _fromIterator(generator());
 }
-function _fromIterable<A>(
+
+export function fromIterable<A>(iterable: Iterable<A>): LazyLinkedList<A> {
+	return () => _fromIterator(iterable[Symbol.iterator]());
+}
+function _fromIterator<A>(
 	iter: Iterator<A>,
 	current: IteratorResult<A> = iter.next()
 ): LazyResult<A> {
-	return current.done ? Nil : LazyCons(current.value, () => _fromIterable(iter));
+	return current.done ? Nil : LazyCons(current.value, () => _fromIterator(iter));
 }
 
-export function chain<A, B>(
-	list: LazyLinkedList<A>,
-	f: (_: A) => LazyLinkedList<B>
-): LazyLinkedList<B> {
+export function map<A, B>(f: (_: A) => B): (list: LazyLinkedList<A>) => LazyLinkedList<B>;
+export function map<A, B>(list: LazyLinkedList<A>, f: (_: A) => B): LazyLinkedList<B>;
+export function map() {
+	return _purry(_map, arguments);
+}
+function _map<A, B>(list: LazyLinkedList<A>, f: (_: A) => B): LazyLinkedList<B> {
 	return () =>
-		match(list, {
+		_match<A, LazyResult<B>>(list, {
 			Nil: () => Nil,
-			LazyCons: (value, next) => concat(f(value), chain(next, f))(),
+			LazyCons: (value, next) => LazyCons(f(value), _map(next, f)),
 		});
 }
 
-export function concat<A>(listA: LazyLinkedList<A>, listB: LazyLinkedList<A>): LazyLinkedList<A> {
-	return () =>
-		match(listA, { Nil: listB, LazyCons: (value, next) => LazyCons(value, concat(next, listB)) });
+export function of<A>(x: A): LazyLinkedList<A> {
+	return () => LazyCons(x, () => Nil);
 }
 
-export function map<A, B>(list: LazyLinkedList<A>, f: (_: A) => B): LazyLinkedList<B> {
-	return () =>
-		match<A, LazyResult<B>>(list, {
-			Nil: () => Nil,
-			LazyCons: (value, next) => LazyCons(f(value), map(next, f)),
-		});
+export function reduce<A, B>(f: (acc: B, _: A) => B, seed: B): (list: LazyLinkedList<A>) => B;
+export function reduce<A, B>(list: LazyLinkedList<A>, f: (acc: B, _: A) => B, seed: B): B;
+export function reduce() {
+	return _purry(_reduce, arguments);
 }
-
-export function reduce<A, B>(list: LazyLinkedList<A>, f: (acc: B, _: A) => B, seed: B): B {
+function _reduce<A, B>(list: LazyLinkedList<A>, f: (acc: B, _: A) => B, seed: B): B {
 	let acc = seed;
 	let current = list();
 	while (current !== Nil) {
@@ -72,6 +116,50 @@ export function reduce<A, B>(list: LazyLinkedList<A>, f: (acc: B, _: A) => B, se
 	return acc;
 }
 
-export function toArray<A>(as: LazyLinkedList<A>): A[] {
-	return reduce(as, (a, b) => a.concat([b]), [] as A[]);
+export function unsafeReduceRight<A, B>(
+	f: (acc: B, _: A) => B,
+	seed: B
+): (list: LazyLinkedList<A>) => B;
+export function unsafeReduceRight<A, B>(
+	list: LazyLinkedList<A>,
+	f: (acc: B, _: A) => B,
+	seed: B
+): B;
+export function unsafeReduceRight() {
+	return _purry(_unsafeReduceRight, arguments);
+}
+function _unsafeReduceRight<A, B>(list: LazyLinkedList<A>, f: (acc: B, _: A) => B, seed: B): B {
+	return _match(list, {
+		Nil: () => seed,
+		LazyCons: (value, next) => f(_unsafeReduceRight(next, f, seed), value),
+	});
+}
+
+export function toArray<A>(list: LazyLinkedList<A>): A[] {
+	return _reduce(list, (a, b) => a.concat([b]), [] as A[]);
+}
+
+export function toIterable<A>(list: LazyLinkedList<A>): Iterable<A> {
+	return {
+		[Symbol.iterator]() {
+			let current = list;
+			return {
+				next() {
+					const result = current();
+					if (result === Nil) return { done: true, value: undefined as any };
+					current = result[1];
+					return { done: false, value: result[0] };
+				},
+			};
+		},
+	};
+}
+
+function _purry(this: any, fn: Function, _args: IArguments) {
+	if (_args.length !== fn.length - 1) return fn.apply(this, _args);
+	const self = this;
+	const args = Array.from(_args);
+	return function _purried(x: any) {
+		return fn.apply(self, [x].concat(args));
+	};
 }
