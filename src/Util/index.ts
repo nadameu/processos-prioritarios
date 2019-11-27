dcba4db025337967f59de42c57ed03761c9e5ae4
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-11-27 13:55:23 -0300

    Teste index

--- src/Util/index.ts
+++ src/Util/index.ts
@@ -1,9 +1,9 @@
-import { Maybe } from '../../adt/Maybe';
+import { Maybe, maybe } from 'adt-ts';
 interface HasNumericIndex<A> {
   [index: number]: A;
 }
 export function index(i: number): <A>(obj: HasNumericIndex<A>) => Maybe<A> {
   return function(obj) {
-    return Maybe.fromNullable(obj[i]);
+    return maybe.fromNullable(obj[i]);
   };
 }
