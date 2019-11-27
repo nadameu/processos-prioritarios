dee2758e962ae2b7f40c0967f1c6056f3abdc8b6
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-11-27 16:17:41 -0300

    Configuração do eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,10 @@ module.exports = {
     'prefer-arrow-callback': 'warn',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }
+    ],
     '@typescript-eslint/no-use-before-define': ['error', 'nofunc']
   }
 };
