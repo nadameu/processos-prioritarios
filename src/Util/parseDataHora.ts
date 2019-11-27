ecd34529cc37a9cce60ae11ce5864a9df237903c
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2019-11-27 16:17:33 -0300

    Testes parseDataHora

--- src/Util/parseDataHora.ts
+++ src/Util/parseDataHora.ts
@@ -1,4 +1,18 @@
-export function parseDataHora(texto: string): Date {
-  const [d, m, y, h, i, s] = texto.split(/\W/g).map(Number);
-  return new Date(y, m - 1, d, h, i, s);
+import { Maybe, Nothing, Just } from 'adt-ts';
+
+export function parseDataHora(texto: string): Maybe<Date> {
+  const partes = texto.match(/^(\d{2})\/(\d{2})\/(\d{4}) (\d{2}):(\d{2}):(\d{2})$/);
+  if (partes === null) return Nothing;
+  const [_, d, m, y, h, i, s] = partes.map(Number);
+  const data = new Date(y, m - 1, d, h, i, s);
+  if (
+    d !== data.getDate() ||
+    m !== data.getMonth() + 1 ||
+    y !== data.getFullYear() ||
+    h !== data.getHours() ||
+    i !== data.getMinutes() ||
+    s !== data.getSeconds()
+  )
+    return Nothing;
+  return Just(data);
 }
