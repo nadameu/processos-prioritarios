c7a6c451a53ac3805dd94ba8da0513f86ddf1d54
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2020-02-04 14:08:17 -0300

    Erro personalizado

--- src/todosNaoNulos.ts
+++ src/todosNaoNulos.ts
@@ -2,7 +2,7 @@ import { particionarNulos } from './particionarNulos';
 
 export async function todosNaoNulos<a>(valores: (a | null)[]): Promise<a[]> {
   const { indicesNulos, validos } = particionarNulos(valores);
-  if (indicesNulos.length > 0)
-    throw new Error(`Erro(s) no(s) índice(s) ${indicesNulos.join(', ')}.`);
+  if (indicesNulos.length === 1) throw new Error(`Erro no índice ${indicesNulos[0]}.`);
+  if (indicesNulos.length > 1) throw new Error(`Erros nos índices ${indicesNulos.join(', ')}.`);
   return validos;
 }
