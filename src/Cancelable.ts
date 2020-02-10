941fa25360220b2496b84629ff3f634d24ccb9e5
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2020-02-10 17:51:44 -0300

    Propriedade "_promise" passa a ser privada

--- src/Cancelable.ts
+++ src/Cancelable.ts
@@ -1,16 +1,16 @@
 export class Cancelable<a> {
-  constructor(public promise: Promise<a>, private _token: { cancel(): void }) {}
+  constructor(private _promise: Promise<a>, private _token: { cancel(): void }) {}
 
   cancel() {
     this._token.cancel();
   }
 
   catch<b = a>(f: (_: any) => b | PromiseLike<b>): Cancelable<a | b> {
-    return new Cancelable(this.promise.catch(f), this._token);
+    return new Cancelable(this._promise.catch(f), this._token);
   }
 
   then<b>(f: (_: a) => b | PromiseLike<b>, g?: (_: any) => b | PromiseLike<b>): Cancelable<b> {
-    return new Cancelable(this.promise.then(f, g), this._token);
+    return new Cancelable(this._promise.then(f, g), this._token);
   }
 
   static all<a, b, c>(xs: [Cancelable<a>, Cancelable<b>, Cancelable<c>]): Cancelable<[a, b, c]>;
@@ -20,7 +20,7 @@ export class Cancelable<a> {
     const { cancel, settled } = aux(xs);
     return new Cancelable(
       Promise.all(
-        xs.map(({ promise }, i) =>
+        xs.map(({ _promise: promise }, i) =>
           promise.then(
             value => {
               settled(i);
