c82361e59d73b3f80ca214a4a6685d45a7c723cc
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2020-09-15 17:35:43 -0300

    Adequação a nova versão do TypeScript

--- src/truthyKeys.ts
+++ src/truthyKeys.ts
@@ -1,6 +1,6 @@
-export function truthyKeys(obj: object): string {
+export function truthyKeys<T>(obj: T): string {
   return Object.entries(obj)
     .map(([key, value]) => (value ? key : null))
-    .filter((x): x is string => x !== null)
+    .filter((x): x is keyof T => x !== null)
     .join(' ');
 }
